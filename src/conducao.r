6aa120f95aed0304dd4635b271261e898d4b09fa
cpmachado <cpmachado@protonmail.com>
2024-03-14 17:24:49 +0000

    Reorganizar o repositório

---
+++ src/conducao.r
@@ -0,0 +1,14 @@
+filename <- "data/conducao.csv"
+limite <- 32
+if (!exists("dados")) {
+  dados <- read.csv(filename)
+}
+dados <- transform(dados, Ni=cumsum(dados$aula))
+plot(
+  dados$Ni,x=as.Date(dados$dia),
+  xaxs="i", yaxs="i",
+  type="s", ylim=c(0, limite + 1),
+  xlab="dia", ylab="aula", main="Aulas de Condução"
+)
+grid(nx = as.numeric(difftime(max(dados$dia), min(dados$dia), unit = "days")) + 1, lty=1, lwd=1)
+abline(h=28, col="blue")
